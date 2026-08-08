77eb71308e4a9211eab66c83219a2e7c628c223c
Muhammad Abdullah <khawajaabdullah018l@gmail.com>
2026-08-08 18:47:19 +0500

    Flask AI integration

--- Week_7_Model_Deployment/app.py
+++ Week_7_Model_Deployment/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
+
+# Load trained model and TF-IDF vectorizer
+model = joblib.load("model.pkl")
+tfidf = joblib.load("tfidf.pkl")
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    message = request.form["message"]
+
+    # Convert text into TF-IDF features
+    message_features = tfidf.transform([message])
+
+    # Make prediction
+    prediction = model.predict(message_features)[0]
+
+    return render_template(
+        "index.html",
+        prediction=prediction,
+        message=message
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
